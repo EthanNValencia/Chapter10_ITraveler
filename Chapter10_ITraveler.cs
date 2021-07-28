495a083638ee6b4543fd11e0797acdb62f59ad50
Ethan <enephew2@mail.valenciacollege.edu>
2021-07-28 13:52:12 -0400

    ITraveler interface that is used with the PersonStudent class.

---
+++ Chapter10_ITraveler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Chapter10_ITraveler
+{
+    public interface ITravelerInterface
+    {
+        public string GetDestination();
+        public string GetStartLocation();
+        public double DetermineMiles();
+    }
+}
